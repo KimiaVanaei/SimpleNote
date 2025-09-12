aece68469adebca91a3a1b0d8e1f10e190e68f44
Amirhossein Alian <alianmail81@gmail.com>
2025-09-12 18:30:25 +0330

    delete some extra file (maybe needed later)

--- app/src/main/java/ir/sharif/simplenote/data/user/UserProfile.kt
+++
@@ -1,7 +0,0 @@
-package ir.sharif.simplenote.data.user
-
-data class UserProfile(
-    val name: String = "",
-    val email: String = "",
-    val avatarUri: String? = null // content:// or file://
-)
