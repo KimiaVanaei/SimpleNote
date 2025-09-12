98c17100ba7156a1544ee332115f608732898f6d
Amirhossein Alian <alianmail81@gmail.com>
2025-09-13 02:57:50 +0330

    Configs for Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     id("androidx.room") version "2.7.2" apply false
+
+    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
