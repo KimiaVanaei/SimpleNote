4381f5ddb92f28dff75195db8e87289ce4555bd0
Amirhossein Alian <alianmail81@gmail.com>
2025-09-13 10:37:43 +0330

    Change ViewModel and UiState for Logging In

--- app/src/main/java/ir/sharif/simplenote/feature/auth/presentation/LoginUiState.kt
+++ app/src/main/java/ir/sharif/simplenote/feature/auth/presentation/LoginUiState.kt
@@ -3,5 +3,8 @@ package ir.sharif.simplenote.feature.auth.presentation
 data class LoginUiState(
     val email: String = "",
     val password: String = "",
-    val showPassword: Boolean = false
+    val showPassword: Boolean = false,
+    val isLoading: Boolean = false,
+    val success: Boolean = false,
+    val error: String? = null
 )
--- app/src/main/java/ir/sharif/simplenote/feature/auth/presentation/LoginViewModel.kt
+++ app/src/main/java/ir/sharif/simplenote/feature/auth/presentation/LoginViewModel.kt
@@ -4,13 +4,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ir.sharif.simplenote.feature.auth.domain.repository.AuthRepository
 
-class LoginViewModel : ViewModel() {
+
+class LoginViewModel(
+    private val repo: AuthRepository
+) : ViewModel() {
+
     var uiState by mutableStateOf(LoginUiState())
         private set
 
     fun onEmailChange(v: String) { uiState = uiState.copy(email = v) }
     fun onPasswordChange(v: String) { uiState = uiState.copy(password = v) }
     fun togglePasswordVisibility() { uiState = uiState.copy(showPassword = !uiState.showPassword) }
+
+    fun login() {
+        viewModelScope.launch {
+            uiState = uiState.copy(isLoading = true, error = null)
+            runCatching {
+                repo.login(uiState.email, uiState.password)
+            }.onSuccess {
+                uiState = uiState.copy(isLoading = false, success = true)
+            }.onFailure { e ->
+                uiState = uiState.copy(isLoading = false, error = e.message)
+            }
+        }
+    }
 }
 
