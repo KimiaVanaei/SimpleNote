e52599c4fa6661133c68aca5acb40abe57a0256f
Amirhossein Alian <alianmail81@gmail.com>
2025-09-14 10:44:07 +0330

    NoteMapper for Remote

---
+++ app/src/main/java/ir/sharif/simplenote/feature/note/data/mapper/NoteRemoteMapper.kt
@@ -0,0 +1,40 @@
+package ir.sharif.simplenote.feature.note.data.mapper
+
+import ir.sharif.simplenote.feature.note.data.local.NoteEntity
+import ir.sharif.simplenote.feature.note.data.remote.NoteDto
+import ir.sharif.simplenote.feature.note.domain.model.Note
+import ir.sharif.simplenote.core.util.toTimestamp
+
+
+// Domain -> Remote DTO
+fun Note.toDto(): NoteDto {
+    return NoteDto(
+        id = if (id == 0) null else id,
+        title = title,
+        description = content
+    )
+}
+
+// Remote DTO -> Domain
+fun NoteDto.toDomain(username: String): Note {
+    return Note(
+        id = id ?: 0,
+        username = username,
+        title = title,
+        content = description,
+        lastEdited = toTimestamp(updated_at ?: created_at),
+        isSynced = true
+    )
+}
+
+// Remote DTO -> Entity
+fun NoteDto.toEntity(username: String): NoteEntity {
+    return NoteEntity(
+        id = id ?: 0,
+        username = username,
+        title = title,
+        content = description,
+        lastEdited = toTimestamp(updated_at ?: created_at),
+        isSynced = true
+    )
+}
