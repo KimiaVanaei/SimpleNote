0df04a828470c28aad635d01d41193742ca4e834
Amirhossein Alian <alianmail81@gmail.com>
2025-09-14 16:06:13 +0330

    some changes on model and entity

--- app/src/main/java/ir/sharif/simplenote/feature/note/data/local/NoteEntity.kt
+++ app/src/main/java/ir/sharif/simplenote/feature/note/data/local/NoteEntity.kt
@@ -1,10 +1,15 @@
 package ir.sharif.simplenote.feature.note.data.local
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "notes")
+@Entity(
+    tableName = "notes",
+    indices = [Index(value = ["serverId", "username"], unique = true)]
+)
 data class NoteEntity(
+    //@PrimaryKey(autoGenerate = true) val id: Int = 0,
     @PrimaryKey(autoGenerate = true) val localId: Int = 0,
     val serverId: Int? = null,
     val title: String,
@@ -13,3 +18,5 @@ data class NoteEntity(
     val isSynced: Boolean,
     val username: String
 )
+
+enum class PendingAction { NONE, CREATE, UPDATE, DELETE }
--- app/src/main/java/ir/sharif/simplenote/feature/note/domain/model/Note.kt
+++ app/src/main/java/ir/sharif/simplenote/feature/note/domain/model/Note.kt
@@ -1,6 +1,7 @@
 package ir.sharif.simplenote.feature.note.domain.model
 
 data class Note(
+    //val id: Int = 0,
     val localId: Int = 0,              // Room PK
     val serverId: Int? = null,         // Django ID
     val title: String,
